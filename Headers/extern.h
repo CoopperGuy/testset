6307fb46b8201ab2b630bd717267a97b43e2c309
DanryuX <danryux1212@gmail.com>
2021-07-14 23:21:39 +0900

    Editor+
    MFC Editor 추가하면서 Header 파일들
    Project 외부에 Headers 폴더 만들어서
    모아놨습니다.

---
+++ Headers/extern.h
@@ -0,0 +1,11 @@
+#ifndef __EXTERN_H__
+#define __EXTERN_H__
+
+extern HWND g_hWnd;
+const int WINCX = 800;
+const int WINCY = 600;
+
+
+
+
+#endif // !__EXTERN_H__
--- Win32Project2/AbstractFactory.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-
-#ifndef __ABSTRACTFACTORY_H__
-#define __ABSTRACTFACTORY_H__
-
-
-#endif // !__ABSTRACTFACTORY_H__
-#pragma once
