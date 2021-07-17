464dfe13b83fa54e95b9e423ca0dd1457992e0c3
pybsgs1234 <53461340+pybsgs1234@users.noreply.github.com>
2021-07-17 22:06:03 +0900

    d
    d

---
+++ Win32Project2/Boss.cpp
@@ -0,0 +1,34 @@
+#include "stdafx.h"
+#include "Boss.h"
+
+
+CBoss::CBoss()
+{
+}
+
+
+CBoss::~CBoss()
+{
+}
+
+HRESULT CBoss::Initialize()
+{
+	return E_NOTIMPL;
+}
+
+int CBoss::Update()
+{
+	return 0;
+}
+
+void CBoss::Late_Update()
+{
+}
+
+void CBoss::Render(HDC _DC)
+{
+}
+
+void CBoss::Release()
+{
+}
---
+++ Win32Project2/Boss.h
@@ -0,0 +1,22 @@
+#pragma once
+#include "Monster.h"
+class CBoss :
+	public CMonster
+{
+public:
+	enum BOSS_STATE { IDLE, ATTACK_1, ATTACK_2, END };
+
+	CBoss();
+	~CBoss();
+
+	// CMonster을(를) 통해 상속됨
+	virtual HRESULT Initialize() override;
+	virtual int Update() override;
+	virtual void Late_Update() override;
+	virtual void Render(HDC _DC) override;
+	virtual void Release() override;
+
+private:
+
+};
+
