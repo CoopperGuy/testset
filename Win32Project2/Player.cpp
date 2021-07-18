33b8fa025262f1274646c915b6ca86d38380b661
CoopperGuy <crisente235@gmail.com>
2021-07-18 22:10:40 +0900

    123123
    123123

--- Win32Project2/Player.cpp
+++ Win32Project2/Player.cpp
@@ -308,14 +308,14 @@ void CPlayer::Shut_Bullet()
 		if (Inven[cur_Weapon].cur_magazine > 0) {
 			Inven[cur_Weapon].cur_magazine--;
 			CObjMgr::Get_Instance()->Add_Object(
-				CAbstractFactory<CLinear_Bullet>::Create(m_tInfo.vPos.x + m_tInfo.vSize.x*1.5f, m_tInfo.vPos.y + m_tInfo.vSize.y*0.25f), OBJID::PLAYERBULLET);
+				CAbstractFactory<CLinear_Bullet>::Create(m_tInfo.vPos.x + m_tInfo.vSize.x*1.5f, m_tInfo.vPos.y - 20.f ), OBJID::PLAYERBULLET);
 		}
 		break;
 	case CPlayer::BULLET_GUIDE:
 		if (Inven[cur_Weapon].cur_magazine > 0) {
 			Inven[cur_Weapon].cur_magazine--;
 			CObjMgr::Get_Instance()->Add_Object(
-				CAbstractFactory<CGuided_Bullet>::Create(m_tInfo.vPos.x + m_tInfo.vSize.x*1.5f, m_tInfo.vPos.y + m_tInfo.vSize.y*0.25f), OBJID::PLAYERBULLET);
+				CAbstractFactory<CGuided_Bullet>::Create(m_tInfo.vPos.x + m_tInfo.vSize.x*1.5f, m_tInfo.vPos.y - 20.f ), OBJID::PLAYERBULLET);
 		}
 		break;
 	}
